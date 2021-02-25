6b6bca09182c09d0a177eaaa3e37b19577c34b81
Sampson Crowley <sampsonsprojects@gmail.com>
2021-02-24 20:42:53 -0700

    design updates for full screen calendly

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useContext, useState } from "react"
+import { useRef, useContext, useState, useEffect } from "react"
 import styles from './App.module.css';
 import { AsyncVideoPlayer } from "components/async-video-player"
 import { LoadingDots } from "components/loading-dots"
@@ -12,36 +12,44 @@ export function App() {
   const formWrapper = useRef(null),
         { isClient } = useContext(ExecutionContext),
         [ calendlyEvent, setCalendlyEvent ] = useState({}),
-        [ schedulingEvent, setSchedulingEvent ] = useState(false),
-        [ showCalendar, setShowCalender ] = useState(false),
+        [ eventScheduled, setEventScheduled ] = useState(/\/event_scheduled/.test(window.location)),
+        [ showCalendar, setShowCalender ] = useState(!!eventScheduled),
+        [ boundingRect, setBoundingRect ] = useState(null),
         isProduction = process.env.NODE_ENV === "production",
-        urlParams = new URLSearchParams(window.location.search),
-        header = urlParams.get('header'),
-        subHeader = urlParams.get('sub-header'),
-        button = urlParams.get('button'),
-        underText = urlParams.get('under');
+        urlParams = new URLSearchParams(window.location.search)
 
-  function scrollToForm(ev) {
-    ev.preventDefault()
-    ev.stopPropagation()
-    if(formWrapper.current) formWrapper.current.scrollIntoView({ block: "start", inline: "start", behavior: "smooth" })
-    setSchedulingEvent(true)
-  }
+  useEffect(() => {
+    if(showCalendar && formWrapper.current) {
+      setBoundingRect(formWrapper.current.getBoundingClientRect())
+    } else {
+      setBoundingRect(null)
+    }
+  }, [ showCalendar, formWrapper.current ])
+
+  useEffect(() => {
+    if(calendlyEvent.event === "calendly.event_scheduled") {
+      try {
+        const url = new URL(calendlyEvent.payload.event.uri),
+              key = /^\/scheduled_events\/[^/]+/.test(url.pathname) ? url.pathname.split("/")[2] : "",
+              path = `/event_scheduled/${key || ""}`
+
+        if(window.history) {
+          window.history.pushState(key || "scheduled", "Event Scheduled!", path)
+          setEventScheduled(key || true)
+        } else {
+          window.location.href = path
+        }
+      } catch(err) {
+        window.location.href = "/event_scheduled"
+      }
+    }
+    /*
+    {"event":"calendly.event_scheduled","payload":{"event":{"uri":"https://api.calendly.com/scheduled_events/CDW7YFUUGSM5BOPY"},"invitee":{"uri":"https://api.calendly.com/scheduled_events/CDW7YFUUGSM5BOPY/invitees/GF5NIVWIEUG64SBZ"}}}
+    */
+  }, [ calendlyEvent ])
 
   function calendlyEventHandler(ev) {
     setCalendlyEvent(ev.data)
-  }
-
-  function scheduleCompleted(ev) {
-    ev && ev.preventDefault()
-
-    setSchedulingEvent(false)
-  }
-
-  function scheduleEvent(ev) {
-    ev && ev.preventDefault()
-
-    setSchedulingEvent(true)
   }
 
   function openCalendarModal(ev) {
@@ -54,6 +62,7 @@ export function App() {
     ev && ev.preventDefault()
 
     setShowCalender(false)
+    setEventScheduled(false)
   }
 
   return (
@@ -63,27 +72,31 @@ export function App() {
     >
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-5 col-lg">
+          <div className="col-12 col-lg-5 d-flex flex-column">
             <header className={`${styles.topHeader} mb-5`}>
               <img src={logo} alt="MoneyDolly" className={styles.topHeaderLogo} />
             </header>
-            <div className={styles.largeLeftPad}>
-              <h5 className={styles.easyHeader}>
-                FUNDRAISING
-                <br/>
-                MADE
-                <br/>
-                EASY
-              </h5>
-              <p className={styles.description}>
-                Covid concerns? No problem! Click below to schedule a no risk,
-                no pressure 10-minute call with Money Dolly, and see how easy
-                fundraising has just become.
-              </p>
+            <div className={`ps-lg-5 flex-fill d-flex flex-column justify-content-center`}>
+              {
+                /*
+                  <h5 className={styles.easyHeader}>
+                    FUNDRAISING
+                    <br/>
+                    MADE
+                    <br/>
+                    EASY
+                  </h5>
+                  <p className={styles.description}>
+                    Covid concerns? No problem! Click below to schedule a no risk,
+                    no pressure 10-minute call with Money Dolly, and see how easy
+                    fundraising has just become.
+                  </p>
+                */
+              }
               <div className="row">
-                <div className="col d-flex justify-content-start mb-5">
+                <div className="col d-flex justify-content-center justify-content-lg-start mb-5">
                   <button
-                    className={styles.scheduleBtn}
+                    className={`${styles.scheduleBtn} flex-lg-fill`}
                     onClick={openCalendarModal}
                   >
                     Schedule a Call
@@ -94,46 +107,58 @@ export function App() {
             </div>
           </div>
           <div className="col d-flex align-items-end justify-content-end">
-            <img className="img-fluid" src={iphone} alt="Get Help from Money Dolley"/>
+            <img
+              className={`${styles.clickable} img-fluid`}
+              src={iphone} alt="Get Help from Money Dolley"
+              onClick={openCalendarModal}
+            />
           </div>
         </div>
       </div>
-      <div class={`modal ${showCalendar ? "d-block" : "d-none"}`} tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-          <div class="modal-content">
-            <div ref={formWrapper} className="card mb-5">
-              <CalendlyEventListener
-                onDateAndTimeSelected={calendlyEventHandler}
-                onEventScheduled={calendlyEventHandler}
-                onEventTypeViewed={calendlyEventHandler}
-                onProfilePageViewed={calendlyEventHandler}
-              >
-                <iframe
-                  title="MoneyDolly Calender Reservations"
-                  frameBorder="0"
-                  width="100%"
-                  height={"300"}
-                  scrolling="no"
-                  src={`https://calendly.com/moneydolly?embed_domain=${document.location.host}&embed_type=PopupWidget`}
-                />
-              </CalendlyEventListener>
-            </div>
+      <div className={`modal ${showCalendar ? "d-block" : "d-none"}`} tabIndex="-1">
+        <div className="modal-dialog modal-dialog modal-fullscreen py-5 px-3 p-md-5">
+          <div className="modal-content">
+            {
+              !!showCalendar
+              && (
+                eventScheduled
+                  ? (
+                      <h1 className=" d-flex flex-column justify-content-center h-100 text-center">
+                        Your Event Has Been Scheduled!
+                      </h1>
+                    )
+                  : (
+                      <div ref={formWrapper} className="card h-100">
+                        {
+                          !!formWrapper.current
+                          && !!showCalendar
+                          && !!boundingRect
+                          && !!boundingRect.height
+                          && (
+                            <CalendlyEventListener
+                              onDateAndTimeSelected={calendlyEventHandler}
+                              onEventScheduled={calendlyEventHandler}
+                              onEventTypeViewed={calendlyEventHandler}
+                              onProfilePageViewed={calendlyEventHandler}
+                            >
+                              <iframe
+                                title="MoneyDolly Calender Reservations"
+                                frameBorder="0"
+                                width="100%"
+                                height={boundingRect.height}
+                                scrolling="no"
+                                src={`https://calendly.com/moneydolly?embed_domain=${document.location.host}&embed_type=PopupWidget`}
+                              />
+                            </CalendlyEventListener>
+                          )
+                        }
+                      </div>
+                    )
+              )
+            }
           </div>
         </div>
       </div>
-      {
-        (!isProduction && false) && (
-          <div className="row mb-5">
-            <div className="col">
-              <div className="card bg-dark text-light mb-5">
-                <div className="card-body">
-                  Calendly Events: { JSON.stringify(calendlyEvent) }
-                </div>
-              </div>
-            </div>
-          </div>
-        )
-      }
       {
         !isClient && (
           <div className={styles.loadingWrapper}>
